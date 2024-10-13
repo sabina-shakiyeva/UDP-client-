51cc912580388cbb8e94e9cc6b74c2daae4dbf40
sabina-shakiyeva <shakiyevasabina@gmail.com>
2024-10-13 07:08:46 +0400

    Add project files.

---
+++ UDP(client)/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+Console.WriteLine("Client Side");
+
+var client = new UdpClient();
+var endPoint = new IPEndPoint(IPAddress.Loopback, 5000);
+
+while (true)
+{
+    Console.WriteLine("Enter File path(or write exit for end):");
+    string filePath = Console.ReadLine();
+
+    if (filePath == "exit")
+    {
+        var msg = Encoding.UTF8.GetBytes("EXIT");
+        client.Send(msg, msg.Length, endPoint);
+        break;
+    }
+
+
+    var startMsg = Encoding.UTF8.GetBytes("start");
+    client.Send(startMsg, startMsg.Length, endPoint);
+
+    using (var readFs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+    {
+        int len = 0;
+        var buffer = new byte[1024];
+        while ((len = readFs.Read(buffer, 0, buffer.Length)) > 0)
+        {
+            client.Send(buffer, len, endPoint);
+        }
+        Console.WriteLine("Photo sent.");
+    }
+
+
+    var endMsg = Encoding.UTF8.GetBytes("end");
+    client.Send(endMsg, endMsg.Length, endPoint);
+}
+
+client.Close();
---
+++ UDP(server)/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+Console.WriteLine("server");
+
+var server = new UdpClient(5000);
+var remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+int count = 0;
+
+while (true)
+{
+    var bytes = server.Receive(ref remoteEndPoint);
+    var msg = Encoding.UTF8.GetString(bytes);
+
+    if (msg == "EXIT")
+    {
+        Console.WriteLine("Images sent");
+        break;
+    }
+
+    if (msg.StartsWith("start"))
+    {
+        count++;
+        string clientFolder = $@"C:\Users\shaki\source\repos\UDP(client)\UDP(server)\{remoteEndPoint.Address}";
+        Directory.CreateDirectory(clientFolder);
+        var destination = $@"{clientFolder}\image_{count}.jpg";
+        using (var fs = new FileStream(destination, FileMode.Create, FileAccess.Write))
+        {
+            while (true)
+            {
+                bytes = server.Receive(ref remoteEndPoint);
+                msg = Encoding.UTF8.GetString(bytes);
+
+                if (msg == "end")
+                {
+                    Console.WriteLine($"Image {count} saved to {destination}");
+                    break;
+                }
+                fs.Write(bytes, 0, bytes.Length);
+            }
+        }
+    }
+}
